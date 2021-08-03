1c2e2acd05e6bfbc0ca0e1aaf4bf4eafe264a7a9
Ariele Mendonça <ariele_apm@outlook.com>
2021-08-02 23:45:48 -0300

    Add home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import api from "./service/api";
+import "./style.css";
+interface IClients {
+  name: string;
+  birth: string;
+  email: string;
+  value: number;
+}
+interface IOperators {
+  name: string;
+  clients: IClients[];
+}
 
 function App() {
+  const [operators, setOperators] = useState<IOperators[]>([]);
+  const [update, setUpdate] = useState(false);
+
+  const isUpdate = async () => {
+    if (operators.length > 0) {
+      await api.get("/redistribute");
+    }
+    setUpdate(!update);
+  };
+
+  useEffect(() => {
+    api.get("/operators").then((response) => setOperators(response.data));
+  }, [update]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header setUpdate={isUpdate} />
+      <Main operators={operators} />
+      <ToastContainer />
+    </>
   );
 }
 
